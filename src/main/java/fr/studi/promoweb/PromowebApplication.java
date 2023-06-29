8a463c42f91d75234302e30686bb5c574736fbfe
Jean-Yves Rosaye <83571685+Credominute@users.noreply.github.com>
2023-06-29 14:53:58 +0200

    implementation de springsecurity

--- src/main/java/fr/studi/promoweb/PromowebApplication.java
+++ src/main/java/fr/studi/promoweb/PromowebApplication.java
@@ -1,7 +1,11 @@
 package fr.studi.promoweb;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 
 @SpringBootApplication
 public class PromowebApplication {
@@ -9,5 +13,4 @@ public class PromowebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PromowebApplication.class, args);
 	}
-
 }
---
+++ src/main/java/fr/studi/promoweb/security/configuration/SecurityConfig.java
@@ -0,0 +1,72 @@
+package fr.studi.promoweb.security.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfig {
+    static final String[] AUTH_WHITE_LIST = {
+            "/api/auth",
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+            "/v2/api-docs/**",
+            "/swagger-resources/**"
+    };
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final LogoutHandler logoutHandler;
+    private final AuthenticationProvider authenticationProvider;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers(AUTH_WHITE_LIST).permitAll()
+                .requestMatchers("/swagger-ui/*").permitAll()
+                .requestMatchers("/v3/**").permitAll()
+                //.requestMatchers("/api/**").authenticated()
+                .anyRequest().authenticated()
+                //.anyRequest().permitAll()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout()
+                .logoutUrl("/api/logout")
+                .addLogoutHandler(logoutHandler)
+                .logoutSuccessHandler(((request, response, authentication) -> SecurityContextHolder.clearContext()));
+        return httpSecurity.build();
+              /*  .csrf().disable()
+                .authorizeExchange()
+                .pathMatchers("/v2/api-docs").permitAll()
+                .pathMatchers("/configuration/ui").permitAll()
+                .pathMatchers("/swagger-resources/**").permitAll()
+                .pathMatchers("/configuration/security").permitAll()
+                .pathMatchers("/swagger-ui.html").permitAll()
+                .pathMatchers("/swagger-ui/*").permitAll()
+                .pathMatchers("/webjars/**").permitAll()
+                .pathMatchers("/v2/**").permitAll()
+                .and().cors()
+                .and().oauth2ResourceServer()
+                .jwt().and().and()
+        return httpSecurity.build();*/
+    }
+}
